afdc170f899b90398eebf1eefae630616e6ca245
Kenneth Reitz <me@kennethreitz.com>
2012-03-04 11:37:44 -0500

    set it up

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='Flask-GoogleFed',
+    version='0.1',
+    url='https://github.com/kennethreitz/flask-googlefed',
+    license='BSD',
+    author='Kennneth Reitz',
+    author_email='me@kennethreitz.com',
+    description='Google Federated Logins for Flask.',
+    long_description=open('README.rst', 'r').read(),
+    py_modules=['flask_googlefed'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-OpenID'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
